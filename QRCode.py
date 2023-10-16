d38e43480c88b9f9e3fbdc37eae973eb7e991286
TaiShow0121 <148000013+TaiShow0121@users.noreply.github.com>
2023-10-16 11:24:43 +0900

    Update QRCode.py

--- QRCode.py
+++ QRCode.py
@@ -11,9 +11,22 @@ qr = qrcode.QRCode(
     border = 4,
 )
 
-qr.add_data("山田 太郎 123 株式会社ABC".encode('utf-8'), optimize=0)
+print("姓:")
+Surname = input()
+print("名:")
+name = input()
+print("番号:")
+Number = input()
+print("会社名:")
+Company = input()
+
+
+qr.add_data((Surname +" "+ name +" "+ Number +" "+ Company).encode('utf-8'), optimize=0)
+# qr.add_data("山田 太郎 123 株式会社ABC".encode('utf-8'), optimize=0)
+
 qr.make(fit=True)
 
 img = qr.make_image(fill='black', back_color='white')
+print("QRCode name:")
 image_name = input()
 img.save(image_name+".png")
